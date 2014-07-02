f44fe8c06b7bfa7d5c14a0290d9bd93ce441ca5b
Don Ky <don.d.ky@gmail.com>
2014-07-02 14:38:10 -0400

    Updated START..END for drive ranges

--- fdisk.sh
+++ fdisk.sh
@@ -2,11 +2,16 @@
 
 DISKPATH=/dev/sd
 
+#Update this start/end range with your drive letters ex: /dev/sdac /dev/sdd...
+STARTDISK=c
+ENDDISk=f
+
+
 read -p "This script is destructive and will wipe out your partitions, Are you sure? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	for i in `echo {c..f}`;
+	for i in `eval echo {$START..$END}`;
 	do
 		dd if=/dev/zero of=$DISKPATH$i bs=512 count=1
 		(echo n; echo p; echo 1; echo 1; echo; echo t; echo fd; echo w) | fdisk $DISKPATH$i
