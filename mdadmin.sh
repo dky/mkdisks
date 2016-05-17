8af0160b1f19f0dfed296a4560ff50150c787f10
Don Ky <don.d.ky@gmail.com>
2016-05-17 09:52:15 -0400

    mdadmin should be 16 disks

--- mdadmin.sh
+++ mdadmin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo mdadm --create /dev/md0 --level=0 --raid-devices=8 /dev/sdc /dev/sdd /dev/sde /dev/sdf /dev/sdg /dev/sdh /dev/sdi /dev/sdj
+sudo mdadm --create /dev/md0 --level=0 --raid-devices=16 /dev/sdc /dev/sdd /dev/sde /dev/sdf /dev/sdg /dev/sdh /dev/sdi /dev/sdj /dev/sdk /dev/sdl /dev/sdm /dev/sdn /dev/sdo /dev/sdp /dev/sdq /dev/sdr
