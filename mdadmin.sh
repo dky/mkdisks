3ebcf07c61faf5840b93479b7217c3c19902b8b8
Don Ky <don.d.ky@gmail.com>
2016-05-17 15:08:58 -0400

    Add mdadmin config

--- mdadmin.sh
+++ mdadmin.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sudo mdadm --create /dev/md0 --level=0 --raid-devices=16 /dev/sdc /dev/sdd /dev/sde /dev/sdf /dev/sdg /dev/sdh /dev/sdi /dev/sdj /dev/sdk /dev/sdl /dev/sdm /dev/sdn /dev/sdo /dev/sdp /dev/sdq /dev/sdr
+
+sudo mkdir /etc/mdadm/
+sudo mdadm --detail --scan > /etc/mdadm/mdadm.conf
